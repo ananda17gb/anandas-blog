03064a28e66b9719d1ce1e88ff8e750a5189a2e8
ananda17gb <ananda.widigdo17@gmail.com>
2025-08-15 15:48:03 +0700

    finishing todos (image uploads when creating blog and signin)

--- anandas-blog-backend/src/routes/api/blogs.ts
+++ anandas-blog-backend/src/routes/api/blogs.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { PrismaClient } from "../../generated/prisma/"
+import { supabase } from "../../config/supabase";
 
 const prisma = new PrismaClient();
 const router = Router();
@@ -7,12 +8,14 @@ const router = Router();
 router.get("/fetch-blogs", async (req, res) => {
   try {
     const blogs = await prisma.blog.findMany();
-    res.status(200).send({
+    res.status(200).json({
+      success: true,
       "message": "Blogs successfully fetched",
       "fetchedBlogs": [...blogs]
     });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
@@ -20,73 +23,93 @@ router.get("/fetch-author-blogs/:authorId", async (req, res) => {
   const authorId = req.params.authorId;
   try {
     const authorBlogs = await prisma.blog.findMany({ where: { authorId } });
-    res.status(200).send({
+    res.status(200).json({
+      success: true,
       "message": "Author's blogs successfully fetched",
       "fetchAuthorBlogs": [...authorBlogs]
     });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
-// TODO: images goes to supabase storage
 router.post("/create-blog", async (req, res) => {
   const { role, title, content, author, authorId, images } = req.body;
-  if (!title || !content || !authorId) return res.status(400).json({ error: "There are missing required fields, please provide all of them to procede creating blogs" })
+  if (!title || !content || !authorId || !author) return res.status(400).json({ error: "There are missing required fields, please provide all of them to procede creating blogs" })
+  if (role !== "AUTHOR" && role !== "ADMIN") return res.status(403).json({ error: "Only admin and author can create blogs" });
   try {
-    if (role !== "AUTHOR" && role !== "ADMIN") return res.status(403).json({ error: "Only admin and author can create blogs" });
+    const newImages: string[] = [];
+    if (images) {
+      if (!Array.isArray(images)) return res.status(400).json({ error: "Images needed to be in an array even though there's only one image" });
+      for (let index = 0; index < images.length; index++) {
+        const element = images[index];
+        const { error: uploadError } = await supabase.storage.from("blog-images").upload(`/blog-images/${Date.now()}_${element}`, element);
+        if (uploadError) {
+          throw uploadError;
+        }
+        const { data: { publicUrl } } = supabase.storage.from("blog-images").getPublicUrl(`/blog-images/${Date.now()}_${element}`);
+        if (publicUrl) { newImages.push(publicUrl); }
+      }
+    }
     const newBlog = await prisma.blog.create({
       data: {
         title: title,
         content: content,
         author: author,
         authorId: authorId,
-        images: images,
+        images: newImages,
         published: true,
       }
     })
-    res.status(201).send({
+    res.status(201).json({
+      success: true,
       "message": "Blog successfully created",
       "createdBlog": { ...newBlog }
     })
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
 router.delete("/delete-blog/:blogId", async (req, res) => {
   const blogId = req.params.blogId;
   const { role, authorId } = req.body;
+  if (role !== "AUTHOR" && role !== "ADMIN") return res.status(403).json({ error: "Only admin and author can delete blogs" });
   try {
-    if (role !== "AUTHOR" && role !== "ADMIN") return res.status(403).json({ error: "Only admin and author can delete blogs" });
     const blog = await prisma.blog.findUnique({ where: { id: blogId } })
     if (!blog) return res.status(404).json({ error: "No blog found" })
     if (blog?.authorId !== authorId && role !== "ADMIN") return res.status(403).json({ error: "This blog is not yours, you can't delete it." })
     await prisma.blog.delete({ where: { id: blogId } })
-    res.status(200).send({
+    res.status(200).json({
+      success: true,
       "message": "Blog successfully deleted",
       "deletedBlog": { ...blog }
     })
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
 router.patch("/edit-blog/:blogId", async (req, res) => {
   const blogId = req.params.blogId;
   const { title, content, images, role, authorId } = req.body
+  if (role !== "AUTHOR" && role !== "ADMIN") return res.status(403).json({ error: "Only admin and author can edit blogs" });
   try {
-    if (role !== "AUTHOR" && role !== "ADMIN") return res.status(403).json({ error: "Only admin and author can edit blogs" });
     const blog = await prisma.blog.findUnique({ where: { id: blogId } })
     if (!blog) return res.status(404).json({ error: "No blog found" })
     if (blog?.authorId !== authorId && role !== "ADMIN") return res.status(403).json({ error: "This blog is not yours, you can't edit it." })
     const updatedBlog = await prisma.blog.update({ where: { id: blogId }, data: { title: title, content: content, images: images } })
-    res.status(200).send({
+    res.status(200).json({
+      success: true,
       "message": "Blog successfully edited",
       "editedBlog": { ...updatedBlog }
     })
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
--- anandas-blog-backend/src/routes/api/user.ts
+++ anandas-blog-backend/src/routes/api/user.ts
@@ -1,12 +1,32 @@
 import { PrismaClient } from "../../generated/prisma/"
 import { Router } from "express";
+import { supabase } from "../../config/supabase";
 
 const prisma = new PrismaClient();
 const router = Router();
 
-// TODO: integrate supabase auth
-router.get("/login", async (req, res) => {
-
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) return res.status(400).json({ error: "Email and password are required" });
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) return res.status(400).json({ error: "Please provide a valid email" });
+  if (password.length < 10) return res.status(400).json({ error: "Please provide a valid password which contains 10 or more characters" });
+  try {
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) {
+      return res.status(401).json({
+        success: false,
+        error: "Invalide credentials"
+      })
+    }
+    res.status(200).json({
+      success: true,
+      "message": "You've successfully signedin"
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message })
+  }
 })
 
 router.get("/fetch-author-data/:authorId", async (req, res) => {
@@ -14,9 +34,14 @@ router.get("/fetch-author-data/:authorId", async (req, res) => {
   try {
     const author = await prisma.user.findUnique({ where: { id: authorId }, omit: { password: true } })
     if (!author) return res.status(404).json({ error: "Author is not found" });
-    res.status(200).send(author);
+    res.status(200).json({
+      success: true,
+      "message": "Author data fetched",
+      "authorData": { ...author }
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message })
   }
 })
 
